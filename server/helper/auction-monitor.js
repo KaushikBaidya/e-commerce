2a8da949b17f41bed8d6eb6377b524ea2da2090e
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2025-06-02 13:19:43 +0600

    okay

--- server/helper/auction-monitor.js
+++ server/helper/auction-monitor.js
@@ -1,41 +1,40 @@
-// socket-service.js or auction-monitor.js
-const AuctionProduct = require("../models/AuctionProduct");
+const Auction = require("../models/AuctionProduct");
 const { getIO } = require("../helper/socket");
 
 function startAuctionMonitor() {
-  setInterval(async () => {
-    const now = new Date();
+	setInterval(async () => {
+		const now = new Date();
 
-    const upcomingToRunning = await AuctionProduct.find({
-      status: "upcoming",
-      startTime: { $lte: now },
-    });
+		const upcomingToRunning = await Auction.find({
+			status: "upcoming",
+			startTime: { $lte: now },
+		});
 
-    const runningToClosed = await AuctionProduct.find({
-      status: "running",
-      endTime: { $lte: now },
-    });
+		const runningToClosed = await Auction.find({
+			status: "running",
+			endTime: { $lte: now },
+		});
 
-    for (const auction of upcomingToRunning) {
-      auction.status = "running";
-      auction.isActive = true;
-      await auction.save();
-      getIO().emit("auctionStatusUpdate", {
-        auctionId: auction._id,
-        status: "running",
-      });
-    }
+		for (const auction of upcomingToRunning) {
+			auction.status = "running";
+			auction.isActive = true;
+			await auction.save();
+			getIO().emit("auctionStatusUpdate", {
+				auctionId: auction._id,
+				status: "running",
+			});
+		}
 
-    for (const auction of runningToClosed) {
-      auction.status = "closed";
-      auction.isActive = false;
-      await auction.save();
-      getIO().emit("auctionStatusUpdate", {
-        auctionId: auction._id,
-        status: "closed",
-      });
-    }
-  }, 5000); // every 5 seconds (tweak as needed)
+		for (const auction of runningToClosed) {
+			auction.status = "closed";
+			auction.isActive = false;
+			await auction.save();
+			getIO().emit("auctionStatusUpdate", {
+				auctionId: auction._id,
+				status: "closed",
+			});
+		}
+	}, 5000);
 }
 
 module.exports = { startAuctionMonitor };
