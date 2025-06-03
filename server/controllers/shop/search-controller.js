babfdd627c2f0c0a8fafbb15686856dc8bc1a774
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2025-06-03 10:19:51 +0600

    okay

--- server/controllers/shop/search-controller.js
+++ server/controllers/shop/search-controller.js
@@ -2,40 +2,41 @@ const Product = require("../../models/Product");
 const sanitize = require("mongo-sanitize");
 
 const searchProducts = async (req, res) => {
-	try {
-		const keyword = sanitize(req.query.keyword);
+  try {
+    const keyword = sanitize(req.params.keyword);
 
-		if (!keyword || typeof keyword !== "string") {
-			return res.status(400).json({
-				succes: false,
-				message: "Keyword is required and must be in string format",
-			});
-		}
+    if (!keyword || typeof keyword !== "string") {
+      return res.status(400).json({
+        success: false,
+        message: "Keyword is required and must be in string format",
+      });
+    }
 
-		const regEx = new RegExp(keyword, "i");
+    const regEx = new RegExp(keyword, "i");
 
-		const createSearchQuery = {
-			$or: [
-				{ title: regEx },
-				{ description: regEx },
-				{ category: regEx },
-				{ brand: regEx },
-			],
-		};
+    const createSearchQuery = {
+      $or: [
+        { title: regEx },
+        { description: regEx },
+        { category: regEx },
+        { brand: regEx },
+      ],
+    };
 
-		const searchResults = await Product.find(createSearchQuery);
+    const searchResults = await Product.find(createSearchQuery);
 
-		res.status(200).json({
-			success: true,
-			data: searchResults,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			success: false,
-			message: "Error",
-		});
-	}
+    res.status(200).json({
+      success: true,
+      data: searchResults,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
 };
+
 
 module.exports = { searchProducts };
