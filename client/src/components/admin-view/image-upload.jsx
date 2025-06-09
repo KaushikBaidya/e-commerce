ef3f231d4508182439f595b903347267c651357a
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2025-06-09 10:41:58 +0600

    ok

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ const ImageUpload = ({
 
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/admin/products/upload-image',
+        `${import.meta.env.VITE_API_BASE_URL}/admin/products/upload-image`,
         data,
         {
           headers: {
